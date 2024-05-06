dbceb82a482aa9253d4e13a345fec5d49ecebb4f
Shivdutt chauhan <128696799+shiv9161@users.noreply.github.com>
2024-05-06 15:24:35 +0530

    adding icons

--- TabBased.js
+++ TabBased.js
@@ -3,13 +3,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Setting from './Setting';
 import Chat from './Chat';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabBased() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Chat') {
+              iconName = focused ? 'chatbox' : 'chatbox-outline'; // Updated icon names
+            } else if (route.name === 'Setting') {
+              iconName = focused ? 'settings' : 'settings-outline'; // Updated icon names
+            }
+
+            // Return Ionicons component with the calculated iconName
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
         <Tab.Screen name="Chat" component={Chat} />
         <Tab.Screen name="Setting" component={Setting} />
       </Tab.Navigator>
